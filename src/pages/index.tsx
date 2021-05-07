2d6b0bd4800e488339eb8d59fbe457c27c6cdf4c
Antonio Carlos <antoniocarlossbcdd@gmail.com>
2021-05-07 17:13:09 -0300

    finalizando ep02

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,46 @@
-import { Header } from "../components/Header";
+import { GetStaticProps } from "next";
+import { api } from "../services/api";
 
-export default function Home() {
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  published_at: string;
+  thumbnail: string;
+  decription: string;
+  file: {
+    url: string;
+    type: string;
+    duration: number;
+  };
+};
+
+type HomeProps = {
+  episodes: Episode[];
+};
+
+export default function Home(props: HomeProps) {
   return (
-   <Header/>
-  )
+    <div>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const episodes = await api.get("episodes", {
+      params: {
+        _limit: 12,
+        _sort: "published_at",
+        _order: "desc",
+      },
+    })
+    .then((response) => response.data);
+
+  return {
+    props: {
+      episodes,
+    },
+    revalidate: 60 * 60 * 8,
+  };
+};
